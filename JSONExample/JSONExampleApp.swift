2b5c4f60af4b1e7782424d9ae78f45c2eae8dfae
sxc <qiuyequ@icloud.com>
2022-10-27 09:51:03 +0800

    Initial Commit

---
+++ JSONExample/JSONExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONExampleApp.swift
+//  JSONExample
+//
+//  Created by Xiaochun Shen on 2022/10/27.
+//
+
+import SwiftUI
+
+@main
+struct JSONExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
